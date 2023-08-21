37115c49ee184e3516b1667546e0b61510c8d88b
cjgav <138142457+cjgav@users.noreply.github.com>
2023-08-22 00:12:36 +0800

    Edit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[date.getMonth()];
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
   let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
@@ -8,94 +25,91 @@ function formatDate(timestamp) {
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
+  let currentDate = date.getDate();
 
-  return `${day} ${hours}:${minutes}`;
+  return `${day}, ${month} ${currentDate}, ${hours}:${minutes}`;
 }
 
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   let day = date.getDay();
-  let days = ["Mon", "Tues", "Wen", "Thu", "Fri", "Sat", "Sun"];
-
   return days[day];
 }
 
-// format date displayed on the overview component
-function formatDate(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-  return `${day} ${hours}:${minutes}`;
-}
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
 
-function formatDay(timestamp) {
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let day = new Date(timestamp * 1000);
-  return days[day.getDay()];
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="forecast-day">${formatDay(forecastDay.time)}</div>
+        <img
+          src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+            forecastDay.condition.icon
+          }.png"
+          width="70px"
+        />
+        <div class="forecast-temp">
+          <span class="forecast-temp-max">${Math.round(
+            forecastDay.temperature.maximum
+          )}°</span>
+          <span class="forecast-temp-min">${Math.round(
+            forecastDay.temperature.minimum
+          )}°</span>
+        </div>
+      </div>
+    `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
-  let apiKey = `ea51f60fe988fca97b66493tbo25f95d`;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
-
+  let apiKey = "ea51f60fe988fca97b66493tbo25f95d";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
-  let data = response.data;
-  let temperatureElement = document.querySelector("#temperatureValue");
-  let cityElement = document.querySelector("#cityName");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidityValue");
-  let windSpeedElement = document.querySelector("#windSpeedValue");
+  let cityElement = document.querySelector("#city");
   let dateElement = document.querySelector("#date");
+  let conditionElement = document.querySelector("#condition");
   let iconElement = document.querySelector("#icon");
+  let temperatureElement = document.querySelector("#temperature");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
-  celsiusTemperature = data.main.temp;
+  celsiusTemp = response.data.temperature.current;
 
-  temperatureElement.innerHTML = Math.round(data.main.temp);
-  cityElement.innerHTML = `${data.name}, ${data.sys.country}`;
-  descriptionElement.innerHTML = data.weather[0].description;
-  humidityElement.innerHTML = data.main.humidity;
-  windSpeedElement.innerHTML = Math.round(data.wind.speed);
-  dateElement.innerHTML = formatDate(data.dt);
+  cityElement.innerHTML = response.data.city;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  conditionElement.innerHTML = response.data.condition.description;
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
-  iconElement.setAttribute("alt", data.weather[0].description);
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 
-  getForecast(data.coord);
+  getForecast(response.data.coordinates);
 }
 
 function search(city) {
   let apiKey = "ea51f60fe988fca97b66493tbo25f95d";
-
-  let unit = "metric";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
@@ -105,7 +119,7 @@ function handleSubmit(event) {
   search(cityInputElement.value);
 }
 
-search("Cebu");
-
 let form = document.querySelector("#search-form");
-addEventListener("submit", handleSubmit);
+form.addEventListener("submit", handleSubmit);
+
+search("Cebu");
